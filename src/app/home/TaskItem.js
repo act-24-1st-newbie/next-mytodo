566783b73016f9abe699151f851ef9f66580be08
snow <rhasya@gmail.com>
2024-04-18 15:44:25 +0900

    fix rendering issue

--- src/app/home/TaskItem.js
+++ src/app/home/TaskItem.js
@@ -1,5 +1,5 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Trash2 } from "lucide-react";
 import { format } from "date-fns";
 
@@ -17,11 +17,16 @@ import TaskCheckbox from "./TaskCheckbox";
  */
 export default function TaskItem({ task }) {
   const [check, setCheck] = useState(task.is_done);
+  const [createdAt, setCreatedAt] = useState(null);
 
   /** @param {React.ChangeEvent<HTMLInputElement>} e */
   function handleChange(e) {
     setCheck(e.currentTarget.checked);
   }
+
+  useEffect(() => {
+    setCreatedAt(format(new Date(task.created_date), "MM/dd HH:mm"));
+  }, []);
 
   return (
     <li className="h-[60px] border-[1px] border-gray-400 dark:border-white rounded-sm flex items-center gap-4 px-4">
@@ -29,7 +34,7 @@ export default function TaskItem({ task }) {
         <TaskCheckbox defaultChecked={check} onChange={handleChange} />
       </form>
       <span className={cn("grow", { "line-through text-gray-400": check })}>{task.contents}</span>
-      <span>{format(task.created_date, "MM/dd HH:mm")}</span>
+      <span>{createdAt ? createdAt : "00/00 00:00"}</span>
       <form action={del.bind(null, task.id)}>
         <SubmitButton variant="destructive" size="icon">
           <Trash2 className="h-4 w-4" />
